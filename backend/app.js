ad1cf35974761662ad329f835b2218c9b1f3bce5
SAKTHISPARROW <89923770+SAKTHISPARROW@users.noreply.github.com>
2024-01-04 21:58:38 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,12 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors(
-    origin:["https://expenses-analyser.vercel.app/"],
-    methods:["POST","GET"],
-    credentials:true
-))
+const corsOptions = {
+  origin: 'https://expenses-analyser.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
