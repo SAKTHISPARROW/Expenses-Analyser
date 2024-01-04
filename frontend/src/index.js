8a11061744200e3d281ba0baf2d2aa3558bd5ae0
sakthi <sakthithirunavu07@gmail.com>
2024-01-04 13:22:17 +0530

    Implemented income and expense analyses

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalProvider } from './context/globalcontext';
+import { GlobalStyle } from './styles/Globalstyle';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </React.StrictMode>
+);
