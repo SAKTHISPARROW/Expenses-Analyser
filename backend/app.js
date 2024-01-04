28c60ab10a3e7cdf5e7a145337579cff11fd476e
SAKTHISPARROW <89923770+SAKTHISPARROW@users.noreply.github.com>
2024-01-04 20:37:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 //middlewares
 app.use(express.json())
 app.use(cors(
-    origin:["https://expenses-analyser-api.vercel.app/"],
+    origin:["https://expenses-analyser.vercel.app"],
     methods:["POST","GET"],
     credentials:true
 ))
