0bf08c0ac5251cfb6c77001577a1bab4b46680b3
sakthi <sakthithirunavu07@gmail.com>
2024-01-09 13:17:01 +0530

    Render Link Updated

--- frontend/src/context/globalcontext.js
+++ frontend/src/context/globalcontext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://expenses-analyser-server.onrender.com/";
 
 
 const GlobalContext = React.createContext()
